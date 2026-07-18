3bbc704e7311db39126a1b1ab6315123d327607f
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-07-18 15:08:13 +1000

    Install Nerd Fonts on NixOS desktop

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,12 +1,21 @@
-# Fonts used by terminals and editors on the Macs.
+# Fonts used by terminals and editors on configured desktops.
+let
+  nerdFonts = pkgs: [
+    pkgs.nerd-fonts.hack
+    pkgs.nerd-fonts.iosevka
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+in
 {
   darwin.base =
     { pkgs, ... }:
     {
-      fonts.packages = [
-        pkgs.nerd-fonts.hack
-        pkgs.nerd-fonts.iosevka
-        pkgs.nerd-fonts.jetbrains-mono
-      ];
+      fonts.packages = nerdFonts pkgs;
+    };
+
+  nixos.desktop =
+    { pkgs, ... }:
+    {
+      fonts.packages = nerdFonts pkgs;
     };
 }
