15159287cb6bf477ef7d34927c15b213c900d04e
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-07-20 08:50:45 +1000

    Prune old Nix generations automatically

--- modules/core/nixos.nix
+++ modules/core/nixos.nix
@@ -1,19 +1,33 @@
 { inputs, config, ... }:
 
+let
+  baseHome = config.home.base;
+in
 {
-  nixos.base = {
-    imports = [ inputs.home-manager.nixosModules.home-manager ];
+  nixos.base =
+    { config, ... }:
+    {
+      imports = [ inputs.home-manager.nixosModules.home-manager ];
 
-    nixpkgs.config.allowUnfree = true;
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+      nixpkgs.config.allowUnfree = true;
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      nix.gc = {
+        automatic = true;
+        dates = "weekly";
+      };
+      systemd.services.nix-gc.preStart = ''
+        ${config.nix.package.out}/bin/nix-env \
+          --profile /nix/var/nix/profiles/system \
+          --delete-generations +10
+      '';
 
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.users.thomashexton.imports = [ config.home.base ];
-  };
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+      home-manager.users.thomashexton.imports = [ baseHome ];
+    };
 
   nixos.workstation.home-manager.users.thomashexton.imports = [ config.home.nixos ];
 
