43feab91b48f03892ef2717465bb05842e015a81
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-07-20 21:41:15 +1000

    Use unstable 1Password package

--- modules/1password.nix
+++ modules/1password.nix
@@ -1,4 +1,19 @@
 {
-  darwin.personal.programs._1password-gui.enable = true;
-  nixos.personal.programs._1password-gui.enable = true;
+  darwin.personal =
+    { pkgs, ... }:
+    {
+      programs._1password-gui = {
+        enable = true;
+        package = pkgs.unstable._1password-gui;
+      };
+    };
+
+  nixos.personal =
+    { pkgs, ... }:
+    {
+      programs._1password-gui = {
+        enable = true;
+        package = pkgs.unstable._1password-gui;
+      };
+    };
 }
