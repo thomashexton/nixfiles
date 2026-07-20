5877ba81bfbb42eca2dd522ff286d9a92be56a54
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-07-20 22:51:20 +1000

    Support downloaded workstation binaries

--- modules/workstation.nix
+++ modules/workstation.nix
@@ -7,6 +7,8 @@
       networking.networkmanager.enable = true;
 
       programs.firefox.enable = true;
+      # Let tools such as Zed run generic Linux binaries downloaded by extensions.
+      programs.nix-ld.enable = true;
       environment.systemPackages = [ pkgs.ghostty ];
     };
 
