1df8548e39a94ee105da8429400b0d0d654af4dc
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-07-20 22:26:45 +1000

    Use Ctrl Alt for moving Plasma windows

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -35,19 +35,7 @@
           fi
 
           switch_shortcut="Alt+$shortcut_number"
-          case "$shortcut_number" in
-            1) shifted_symbol='!' ;;
-            2) shifted_symbol='@' ;;
-            3) shifted_symbol='#' ;;
-            4) shifted_symbol='$' ;;
-            5) shifted_symbol='%' ;;
-            6) shifted_symbol='^' ;;
-            7) shifted_symbol='&' ;;
-            8) shifted_symbol='*' ;;
-            9) shifted_symbol='(' ;;
-            0) shifted_symbol=')' ;;
-          esac
-          window_shortcut="Alt+$shifted_symbol"
+          window_shortcut="Ctrl+Alt+$shortcut_number"
 
           "$kwrite" --file "$HOME/.config/kglobalshortcutsrc" --group kwin \
             --key "Switch to Desktop $desktop" \
