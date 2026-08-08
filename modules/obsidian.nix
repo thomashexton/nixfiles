6aef42835373058195b7119b5c49bbb981b49354
Thomas Hexton <25544371+thomashexton@users.noreply.github.com>
2026-08-08 22:41:09 +1000

    Pin Obsidian on Darwin

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -2,7 +2,15 @@
   darwin.workstation =
     { pkgs, ... }:
     {
-      environment.systemPackages = [ pkgs.obsidian ];
+      environment.systemPackages = [
+        (pkgs.obsidian.overrideAttrs {
+          version = "1.12.7";
+          src = pkgs.fetchurl {
+            url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.12.7/Obsidian-1.12.7.dmg";
+            hash = "sha256-O4XBO0zlVRLobhcKfNKklOLbaVrIiMBgHhU8uFt3iBs=";
+          };
+        })
+      ];
     };
 
   nixos.workstation =
